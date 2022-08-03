4ac05554be1f22cb8453e681868afda9e4450315
Alexander Eldeib <alexeldeib@gmail.com>
2022-08-03 02:26:26 +0200

    docs

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Driver container image for AKS VHD
 
-Run with 
+This repo provides steps to build a container image with all components required for 
+Kubernetes Nvidia GPU integration. Run it as a privileged container in the host PID namespace.
+It will enter the host mount namespace and install the nvidia drivers, container runtime, 
+and associated libraries on the host, validating their functionality
 
+## Build
+```
+docker build -f Dockerfile  -t docker.io/alexeldeib/aks-gpu:latest .
+docker push docker.io/alexeldeib/aks-gpu:latest
+```
+
+## Run
 ```bash
 ctr run --privileged --net-host --with-ns pid:/proc/1/ns/pid --mount type=bind,src=/opt/gpu,dst=/mnt/gpu,options=rbind --mount type=bind,src=/opt/actions,dst=/mnt/actions,options=rbind -t docker.io/alexeldeib/aks-gpu:latest /entrypoint.sh install.sh
 ```
+
+Note the `--with-ns pid:/proc/1/ns/pid` and `--privileged`, as well as the bind mounts, these are key.
--- download.sh
+++ download.sh
@@ -12,13 +12,16 @@ GPU_DEST="/usr/local/nvidia"
 workdir="$(mktemp -d)"
 pushd "$workdir" || exit
 
+# download nvidia drivers, move to permanent cache
 curl -fsSLO https://us.download.nvidia.com/tesla/${DRIVER_VERSION}/NVIDIA-Linux-x86_64-${DRIVER_VERSION}.run 
 mv NVIDIA-Linux-x86_64-${DRIVER_VERSION}.run /opt/gpu/NVIDIA-Linux-x86_64-${DRIVER_VERSION}.run
 
+# download fabricmanager for nvlink based systems, e.g. multi instance gpu vms.
 curl -fsSLO https://developer.download.nvidia.com/compute/cuda/redist/fabricmanager/linux-x86_64/fabricmanager-linux-x86_64-${DRIVER_VERSION}-archive.tar.xz
 tar -xvf fabricmanager-linux-x86_64-${DRIVER_VERSION}-archive.tar.xz
 mv fabricmanager-linux-x86_64-${DRIVER_VERSION}-archive /opt/gpu/fabricmanager-linux-x86_64-${DRIVER_VERSION}
 
+# configure nvidia apt repo to cache packages
 curl -fsSLO https://nvidia.github.io/nvidia-docker/gpgkey
 gpg --dearmor -o aptnvidia.gpg gpgkey
 mv aptnvidia.gpg /etc/apt/trusted.gpg.d/aptnvidia.gpg
@@ -26,11 +29,14 @@ curl -fsSL https://nvidia.github.io/nvidia-docker/ubuntu${VERSION_ID}/nvidia-doc
 
 apt update
 
+# download nvidia debian packages for nvidia-container-runtime compat
 for apt_package in $NVIDIA_PACKAGES; do
     apt-get download ${apt_package}=${NVIDIA_CONTAINER_TOOLKIT_VER}*
     mv ${apt_package}_${NVIDIA_CONTAINER_TOOLKIT_VER}* /opt/gpu
 done
 apt-get download nvidia-container-runtime=${NVIDIA_CONTAINER_RUNTIME_VERSION}*
+
+# move debs to permanent cache
 mv nvidia-container-runtime_${NVIDIA_CONTAINER_RUNTIME_VERSION}* /opt/gpu
 
 popd || exit
